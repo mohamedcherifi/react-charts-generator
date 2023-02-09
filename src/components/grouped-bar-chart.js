479c7458e5617595d41ed476ae7ab9e65b68eeb0
Mohamed Cherifi <mohamed.cherifi@unity3d.com>
2023-02-09 14:37:56 -0500

    fixed npm start

--- src/components/grouped-bar-chart.js
+++ src/components/grouped-bar-chart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'chart.js/auto';
 import { Bar } from "react-chartjs-2";
 
 const GroupedBarChart = () => {
--- src/components/pie-chart.js
+++ src/components/pie-chart.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'chart.js/auto';
 import { Pie } from "react-chartjs-2";
 
 const PieChart = () => {
